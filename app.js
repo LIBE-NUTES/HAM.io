499a4bc3beeb70d3bd89407661991a8191d8d5ae
yasser nascimento <yassern@protonmail.com>
2017-03-04 19:30:23 -0300

    refactoring - ble module

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
 var express = require('express');
 var path = require('path');
-var noble = require('noble');
-var util = require('util');
-var blePacket = require('ble-packet');
 
 var app = express();
 
 var server = require("http").Server(app);
 var io = require('socket.io')(server);
 
-function stateChanged(state) {
-  if (state === 'poweredOn') {
-    console.log('Starting Scanning');
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
-}
+var ble = require('./ble.js');
+ble.start();
 
-function discoveredPeripheral(peripheral) {
-  console.log('Peripheral Discovered');
+ble.getPeripheral(function(peripheral) {
   io.emit('name', peripheral.advertisement.localName);
   io.emit('rssi', peripheral.rssi);
-  peripheral.connect(function (error) {
-    console.log('Device Connected');
-    peripheral.discoverServices(['1809'], discoveredSer);
+
+  ble.getMeasurements(peripheral, function(measurement) {
+    io.emit('measurement', measurement);
   });
-}
-
-function discoveredSer(error, services) {
-  console.log('Sevices Discovered');
-  services[0].once('characteristicsDiscover', discoveredChar);
-  services[0].discoverCharacteristics();
-}
-
-function discoveredChar(characteristics) {
-  console.log('Characteristics Discovered');
-  characteristics[0].notify(true);
-
-  characteristics[0].on('data', function(data, isNotification) {
-    blePacket.parse('0x2a1c', data, function (err, result) {
-      io.emit('measurement', result.tempC)
-    });
-  });
-}
-
-noble.on('stateChange', stateChanged);
-noble.on('discover', discoveredPeripheral);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/vue', express.static(__dirname + '/node_modules/vue/dist/'));
---
+++ ble.js
@@ -0,0 +1,57 @@
+var noble = require('noble');
+var parser = require('ble-packet');
+
+var ble = {};
+
+ble.start = function() {
+  noble.on('stateChange', stateChanged);
+
+  function stateChanged(state) {
+    if (state === 'poweredOn') {
+      console.log('Starting Scanning');
+      noble.startScanning();
+    } else {
+      noble.stopScanning();
+    }
+  }
+}
+
+ble.getPeripheral = function(callback) {
+  noble.on('discover', peripheralDiscovered);
+
+  function peripheralDiscovered(peripheral) {
+    console.log('Peripheral Discovered ' + peripheral.uuid);
+    callback(peripheral);
+  }
+}
+
+ble.getMeasurements = function(peripheral, callback) {
+  peripheral.connect(function(error) {
+    console.log('Device Connected');
+    peripheral.discoverServices(['1809'], servicesDiscovered);
+  });
+
+  function servicesDiscovered(error, services) {
+    var service = services[0];
+    console.log('Sevices Discovered ' + service.uuid);
+    service.on('characteristicsDiscover', characteristicsDiscovered);
+    service.discoverCharacteristics();
+  }
+
+  function characteristicsDiscovered(characteristics) {
+    characteristic = characteristics[0];
+    console.log('Characteristics Discovered ' + characteristic.uuid);
+    characteristic.subscribe();
+    characteristic.on('data', dataReceived);
+  }
+
+  function dataReceived(data, isNotification) {
+    console.log('Buffer ' + data);
+    var uuidCode = '0x' + characteristic.uuid;
+    parser.parse(uuidCode, data, function (err, result) {
+      callback(result.tempC);
+    });
+  }
+}
+
+module.exports = ble;
